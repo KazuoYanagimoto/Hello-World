eb159b27e842f3a42d8e04c46cc5e5ec3f66876e
kazuoyanagimoto <kazuo.yanagimoto@gmail.com>
2015-04-03 21:42:19 +0900

    Remove grunt/

--- grunt/concurrent.js
+++
@@ -1,6 +0,0 @@
-// Run grunt tasks concurrently
-// https://github.com/sindresorhus/grunt-concurrent
-module.exports = {
-	first: ['compass:dev', 'imagemin', 'svgmin'],
-	second: ['uglify:dev']
-};
--- grunt/nodeunit.js
+++
@@ -1,5 +0,0 @@
-// Run Nodeunit unit tests.
-// https://github.com/gruntjs/grunt-contrib-nodeunit
-module.exports = {
-	all: ['test/*_test.js']
-};
