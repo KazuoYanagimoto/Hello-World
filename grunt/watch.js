38b7652427797ff0d7ef9f30ff8575384b741414
kazuoyanagimoto <kazuo.yanagimoto@gmail.com>
2015-04-04 16:20:12 +0900

    Update grunt tasks

--- grunt/watch.js
+++ grunt/watch.js
@@ -1,34 +1,30 @@
 // Watches files for changes and runs tasks based on the changed files
 // https://github.com/gruntjs/grunt-contrib-watch
 module.exports = {
-	scripts: {
-		files: ['<%= config.dev %>/scripts/**/*.js'],
-		tasks: ['newer:uglify:dev']
-	},
-	styles: {
-		files: ['<%= config.dev %>/styles/scss/**/*.scss'],
-		tasks: ['compass:dev', 'autoprefixer:dev']
-	},
-	styles: {
-		files: ['<%= config.dev %>/styles/css/**/*.css'],
-		tasks: ['cssmin:dev']
-	},
-	images: {
-		files: ['<%= config.dev %>/images/**/*'],
-		tasks: ['imagemin', 'svgmin']
-	},
 	fonts: {
 		files: ['<%= config.dev %>/fonts/**/*'],
 		tasks: ['copy:dev']
+	},
+	images: {
+		files: ['<%= config.dev %>/images/**/*.{png,jpg,gif,svg}'],
+		tasks: ['newer:imagemin', 'newer:svgmin']
 	},
 	includes: {
 		files: ['<%= config.dev %>/includes/*.html', '<%= config.dev %>/*.html'],
 		tasks: ['includes']
 	},
+	scripts: {
+		files: ['<%= config.dev %>/scripts/**/*.js'],
+		tasks: ['uglify:dev']
+	},
+	styles: {
+		files: ['<%= config.dev %>/styles/**/*.scss'],
+		tasks: ['compass:dev', 'autoprefixer:dev']
+	},
 	livereload: {
 		options: {
 			livereload: true
 		},
-		files: ['<%= config.tmp %>/scripts/*.js', '<%= config.tmp %>/styles/*.css', '<%= config.tmp %>/images/*', '<%= config.tmp %>/*.html']
+		files: ['<%= config.tmp %>/fonts/**/*', '<%= config.tmp %>/images/**/*', '<%= config.tmp %>/scripts/*.js', '<%= config.tmp %>/styles/*.css', '<%= config.tmp %>/*.html']
 	}
 };
